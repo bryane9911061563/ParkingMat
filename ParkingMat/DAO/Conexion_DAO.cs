af8b58f5e4afae69a2bb2764b10911213a93ad37
palma98jared <48363106+palma98jared@users.noreply.github.com>
2019-03-09 15:52:10 -0600

    conex

---
+++ ParkingMat/DAO/Conexion_DAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ref
+using MySql.Data.MySqlClient;
+
+namespace MiniProyectoABC.DAO
+{
+    class Conexion_DAO
+    {
+        MySqlConnection conex;
+        public MySqlConnection ConectarBD()
+        {
+            string cadena = "datasource = localhost; username= root; password=123456; database=proyectoabc;";
+            conex = new MySqlConnection(cadena);
+            return conex;
+        }
+
+        public void abrirBD()
+        {
+            conex.Open();
+        }
+
+        public void cerrarBD()
+        {
+            conex.Close();
+        }
+
+    }
+
+}
