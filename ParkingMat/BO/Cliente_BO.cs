b8e69c1d654a6d103e97c3a675a8646ed2d20e2a
JaredP <jaredroniano_10@hotmail.com>
2019-03-12 22:38:13 -0600

    clases bo y dao
    bo estan completas y dao solo es cascaron

---
+++ ParkingMat/BO/Cliente_BO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMat.BO
+{
+    class Cliente_BO
+    {
+        private int id_cliente;
+        private string nombre;
+        private string apellido_m;
+        private string apellido_p;
+        private int id_sucursal;
+
+
+        public int Id_cliente { get => id_cliente; set => id_cliente = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido_m { get => apellido_m; set => apellido_m = value; }
+        public string Apellido_p { get => apellido_p; set => apellido_p = value; }
+        public int Id_sucursal { get => id_sucursal; set => id_sucursal = value; }
+    }
+}
---
+++ ParkingMat/BO/Recibo_BO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMat.BO
+{
+    class Recibo_BO
+    {
+        private int folio;
+        DateTime fecha_reg;
+        private int precio;
+        private string tipo;
+        private int id_cliente;
+        private int id_sucursal;
+
+
+
+        public int Folio { get => folio; set => folio = value; }
+        public DateTime Fecha_reg { get => fecha_reg; set => fecha_reg = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public int Id_cliente { get => id_cliente; set => id_cliente = value; }
+        public int Id_sucursal { get => id_sucursal; set => id_sucursal = value; }
+    }
+}
